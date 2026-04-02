da2838b552780a46518662321bc587df4e168035
shaff <shaff@ECOLOGIC>
2026-04-02 22:55:47 +0600

    Add project files.

---
+++ EcologicInnovations.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EcologicInnovations.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
